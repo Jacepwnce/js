a7b7994c8700e786b80a3041704db157b9179f38
Anton Bekhtgold <jacepwncere@yandex.ru>
2021-12-13 07:39:50 +0300

    Работа с операторами

--- js/script.js
+++ js/script.js
@@ -36,3 +36,56 @@ console.log(undef)
 console.log(typeof undef)
 
 //оператор typeof определяет тип данных 
+
+
+
+// next lesson//
+
+
+ //NUMBER START
+
+
+var num1 = 12
+var num2 = 8
+
+
+// можно выполнить замену данных переменной путём обращения к ней по её имени 
+num2 = 2 // Таким образом произошла заменна данных в переменной var num2 
+//
+
+console.log('+:',num1 + num2) //оператор (+) выполняет действие сложения
+
+console.log('-:',num1 - num2) //оператор (-) выполняет действие вычетания
+
+console.log('*:',num1 * num2) //оператор (*) выполняет действие умножения
+
+console.log('/:',num1 / num2) //оператор (/) выполняет действие деления
+
+//NUMBER END
+
+
+// STRING START
+
+var str1 = 'Hello'
+var str2 = 'World'
+
+
+
+console.log('+:', str1 + str2) // Это действие называется конкатенация  (JavaScript метод concat() используется для объединения двух, или более строк в одну, при этом метод не изменяет существующие строки, а возвращает новую строку.) Скопированно из сети....
+
+
+// console.log('+:', str1 + str2) при выполнение данной операции выводится HelloWorld тк, произошла конкатенация и чтобы добавить пробел межлу словами нужно добавить ещё одну строку ('') и внутри строчки в качестве символа поставить пробел (' ')
+
+console.log('+:', str1 + ' ' + str2)
+
+console.log('1+2:', + 1 + '2')  // если испл number в '' то js будет видеть его как объект конкатенации 
+
+
+console.log('Boolean + string:', true + str2)// выводит в консоль tureWorld
+console.log('Boolean + string:', false + str1)// выводит falseHello
+
+console.log('Boolean + number:', true + 1)//2
+console.log('Boolean + number:', false + 1)//1
+console.log('Boolean + number:', true - 1)//0
+console.log('Boolean + number:', false - 1)//-1
+
