41e92ba0cf28852ce3ab0c47c4852e8be5c6ea46
Anton Bekhtgold <jacepwncere@yandex.ru>
2021-12-13 14:21:25 +0300

    TASK-1 start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log ('Hello World!')
+// var carName = 'Ford'
+// var carYear = 2010
+// var personYear = 1990
+
+// function calculateAge (year) {
+//     var currentYear = 2021  
+//     var result = currentYear - year 
+//     return result
+// }
+
+// function checkAndLogAge (year) {
+//     if (calculateAge(year) <= 10) {
+//         console.log('Возраст меньше 10 лет')
+//     } else {
+//         console.log('Возраст больше 10 лет')
+//     }
+// }
+
+// checkAndLogAge(carYear)
+// checkAndLogAge(personYear)
+
+    // if (calculateAge(carYear) <= 10) {
+    //     console.log('Возраст меньше 10 лет')
+    // } else {
+    //     console.log('Возраст больше 10 лет')
+    // }
+
+
+    // if (calculateAge(personYear) <= 18) {
+    //     console.log('Человек не может водить машину')
+    //         } else {
+    //             console.log('Человек может водить машину')
+    //         } 
+
+
+ // TASK-1 //
+
+
+
