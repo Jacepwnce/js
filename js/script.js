fc5384412f4ae0296e1f3f73585cdfd93131f642
Anton Bekhtgold <jacepwncere@yandex.ru>
2021-12-13 06:35:49 +0300

    var

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@
 
 var messsage = 'Hello World !'
 
+var Message = 'This is anotehr var'
+
+var helloMessage = 'Hrllo again' //using camelCase
+
+var $ = 'dollar'
+
+var _ = 'underscore'
+
 console.log(messsage)
+
