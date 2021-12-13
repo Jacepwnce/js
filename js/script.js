68a2a61170bd99ca0dbbf7abc183db430069f0eb
Anton Bekhtgold <jacepwncere@yandex.ru>
2021-12-13 07:02:24 +0300

    data-types

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log ('Hello World!')
+/*
+Number - числа
+String - строчка
+Boolean - содержит в себе только 2 типа данных (False) or (True)
+Object - самый основной тип данных 
+Null
+Undefined 
+*/
+
+
+
+var number = 42
+var string = 'Message'
+var isTrue = true // false
+var obj = {a: 1}
+var isNull = null
+var undef =  undefined
+
+
+console.log(number)
+console.log(typeof number)
+
+console.log(string)
+console.log(typeof string)
+
+console.log(isTrue)
+console.log(typeof isTrue)
+
+console.log(obj)
+console.log(typeof obj)
+
+console.log(isNull)
+console.log(typeof isNull) //При определении типа данных для типа Null - выводится Object - небольшая не точность в языке
+
+console.log(undef)
+console.log(typeof undef)
+
+//оператор typeof определяет тип данных 
