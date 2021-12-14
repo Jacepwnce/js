73cc7ad829e3e05da0e95f8c1c74d279d38fee7b
Anton Bekhtgold <jacepwncere@yandex.ru>
2021-12-14 07:09:29 +0300

    Numbers - rdy

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log ('Hello World!')
+console.log(42)
+console.log(-42)
+console.log(1.5)
+console.log(-8 / 3)// 2.66666
+console.log(0xAB)
+console.log(2e3)//2000
+console.log(NaN)// not a number
+console.log(typeof NaN)
+console.log(123 / 0)
+
+var fortyTwo = 42
+var delta = 8 / 3
+ 
+console.log(fortyTwo.toString())
+console.log(+delta.toFixed(1) + 4)// (+) ствится для перевода числа из STRING в NUMBERS
+console.log(parseFloat(delta.toFixed(2)))// parseFloat для перевода числа из STRING в NUMBERS
+console.log(parseInt(delta.toFixed(2)))// оставляет число без десятичных... работает только с целыми чилами
+
+console.log(isNaN(NaN))
+console.log(isNaN(42))
+
+
+console.log(isFinite(1 / 0))
+console.log(isFinite(9999999))
