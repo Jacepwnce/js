be887903a5f848cdc2d0f7fbce5bf74370738321
Anton Bekhtgold <jacepwncere@yandex.ru>
2022-01-16 11:17:09 +0300

    learn string

---
+++ index.js
@@ -0,0 +1,68 @@
+//https://github.com/airbnb/javascript
+
+
+// let country = "USA"
+// console.log(country) 
+// console.log(country.length)
+
+
+// function getAddressLength(address){
+//     return address.length
+// }
+
+// // console.log(getAddressLength('USA'))
+// // The function WORKS but we don't see the console.log
+// function double(number) {
+//   console.log(number * 2) // we don't see this
+//   return number * 2 // "result" of the function, will exit the function
+ 
+// }
+
+// let result = double(4) // result is 8 - but nothing logged in the Consolenode
+
+
+// function getNextCentury(year) {
+// 	// your code
+//     console.log(year + 100)
+//     return year + 100
+// }
+
+// // Sample usage - Do not modify
+// getNextCentury(2020) // 2120
+// getNextCentury(3000) // 3100
+
+// function sum(a, b) {
+// 	return a + b
+// }
+
+// // Sample usage (do not modify)
+// sum(3, 5) // will return 8 (a = 3, b = 5)
+// sum(4, 1) // will return 5 (a = 4, b = 1)
+
+
+// let name = "Alex"
+// let shouting = name.toUpperCase()
+
+// console.log(shouting)
+// console.log(name)
+
+
+// function getUpperCaseName(name){
+// 	// your code
+//     return name.toUpperCase()
+// }
+
+// // Sample usage (do not modify)
+// console.log(getUpperCaseName("sam")) // SAM
+// console.log(getUpperCaseName("alex")) // ALEX
+// console.log(getUpperCaseName("Johnny")) // JOHNNY
+
+// function getLowerCaseName(name) {
+// 	// your code
+//     return name.toLowerCase()
+// };
+
+// // Sample usage (do not modify)
+// console.log(getLowerCaseName("SAM"))
+// console.log(getLowerCaseName("ALEX"))
+// console.log(getLowerCaseName("JoHNnY"))
