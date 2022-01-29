3c5f6935ab76231c0952bb83c9d12481f3d3462d
Anton Bekhtgold <jacepwncere@yandex.ru>
2022-01-29 18:51:50 +0300

    hw-1 done

--- index.js
+++ index.js
@@ -9,19 +9,19 @@
 // ('/', 49, 7) --> 7
 
 // задача 1
-function calculator (Operator, value1, value2) {
-    if (Operator === '+') {
-        return value1 + value2
-    } else if(Operator === '-') {
-        return value1 - value2
-    }
-    else if (Operator === '*') {
-        return value1 * value2
-    }
-    else if (Operator === '/') {
-        return value1 / value2
-    }
-}
+// function calculator (Operator, value1, value2) {
+//     if (Operator === '+') {
+//         return value1 + value2
+//     } else if(Operator === '-') {
+//         return value1 - value2
+//     }
+//     else if (Operator === '*') {
+//         return value1 * value2
+//     }
+//     else if (Operator === '/') {
+//         return value1 / value2
+//     }
+// }
 
 // Задача
 // Реализуйте функцию squareOfSum(), которая принимает на вход два числа и возвращает квадрат суммы этих чисел. Для вычисления значения используйте формулу из курса алгебры: a² + 2 * a * b + b².
@@ -29,11 +29,11 @@ function calculator (Operator, value1, value2) {
 // squareOfSum(2, 3) // 25
 // squareOfSum(1, 10) // 121
 
-function squareOfSum(a, b){
-    return (a * a) + 2 * a * b + (b * b)
-}
-console.log(squareOfSum(2, 3))
-console.log(squareOfSum(1, 10))
+// function squareOfSum(a, b){
+//     return (a * a) + 2 * a * b + (b * b)
+// }
+// console.log(squareOfSum(2, 3))
+// console.log(squareOfSum(1, 10))
 
 
 
@@ -50,21 +50,76 @@ console.log(squareOfSum(1, 10))
 // 75, если оценка за экзамен выше 50 и есть как минимум 2 проекта
 // 0 в любом другом случае
 
-function finalGrade (exam, projects) {
-    if (exam > 90 || projects > 10 ) {
-        return 100
-    }
-    else if (exam > 75 && projects >= 5) {
-        return 90
-    }
-    else if (exam > 50 && projects >= 2) {
-        return 75
-    }
-    else {
-        return 0
-    }
-    }
-  console.log(finalGrade(96,12))
-  console.log(finalGrade(82,5))
-  console.log(finalGrade(60,4))
-  console.log(finalGrade(30,2))
+// function finalGrade (exam, projects) {
+//     if (exam > 90 || projects > 10 ) {
+//         return 100
+//     }
+//     else if (exam > 75 && projects >= 5) {
+//         return 90
+//     }
+//     else if (exam > 50 && projects >= 2) {
+//         return 75
+//     }
+//     else {
+//         return 0
+//     }
+//     }
+//   console.log(finalGrade(96,12))
+//   console.log(finalGrade(82,5))
+//   console.log(finalGrade(60,4))
+//   console.log(finalGrade(30,2))
+
+// Реализуйте функцию, которая извлекает из массива элемент по указанному индексу, если индекс существует, либо возвращает значение по-умолчанию. Функция принимает на вход три аргумента:
+// Массив
+// Индекс
+// Значение по умолчанию (равно null)
+// const cities = ['moscow', 'london', 'berlin', 'porto'];
+// get(cities, 1); // 'london'
+// get(cities, 4); // null
+// get(cities, 10, 'paris'); // 'paris'
+// get(cities, -1, 'oops'); // 'oops'
+
+
+// function getDistance (speed, time){
+//     let distance = speed * time;
+//     return distance;
+// }
+
+// let myDistance = getDistance (5, 10);
+// console.log(myDistance)
+
+// speedlimit = 51
+
+// function amISpeeding(speed){
+//     if (speed >= speedlimit){
+//         console.log("Yes. You`re speedind");
+//     } else {
+//         (speed <= speedlimit)
+//             console.log("NO. You`re not speeding");
+//     } 
+//     }
+// amISpeeding (48)
+// amISpeeding (85)
+
+// let xPos = 300;
+// let yPos = 150;
+
+// function sendWarning (x, y){
+//     if  ((x < xPos) && (y < yPos)){
+//         console.log("Adjust the poition");
+//     } else {
+//         console.log ('things, fine');
+//     }
+// }
+
+// sendWarning (500, 160)
+// sendWarning (100, 100)
+// sendWarning (201, 149)
+
+
+// Напишите функцию,  которая разбивает слова в регистре.
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// "" =>  ""
+
+
