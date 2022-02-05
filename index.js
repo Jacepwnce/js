21767a590d7c25f7984349c0979e28ed7c86f43a
Anton Bekhtgold <jacepwncere@yandex.ru>
2022-02-05 18:48:29 +0300

    back

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-//Реализуйте функцию, которая высчитывает сумму всех элементов массива, которые делятся без остатка на 3.
+// Реализуйте функцию, которая извлекает из массива элемент по указанному индексу, если индекс существует, либо возвращает значение по-умолчанию. Функция принимает на вход три аргумента:
+// Массив
+// Индекс
+// Значение по умолчанию (равно null)
+// const cities = ['moscow', 'london', 'berlin', 'porto'];
+// get(cities, 1); // 'london'
+// get(cities, 4); // null
+// get(cities, 10, 'paris'); // 'paris'
+// get(cities, -1, 'oops'); // 'oops'
 
-const coll1 = [8, 9, 21, 19, 18, 22, 7]; 
-calculateSum(coll1); // 48 
-const coll2 = [2, 0, 17, 3, 9, 15, 4] 
+const cities = ['Moscow', 'London', 'Berlin', 'Porto']
 
-calculateSum(coll2); // 27
-
-
-const coll1 = [8, 9, 21, 19, 18, 22, 7]
-const coll2 = [2, 0, 17, 3, 9, 15, 4]
-let result = 0
-
-function calculateSum(items) {
-    items.forEach(function (item) {
-        if (item % 3 === 0) {
-            result += item
-        }
-    })
-    return result
+function get(cities, index, def = null) {
+  if (index >= cities.length || index < 0) { // Здесь прописываем инструкцию для массива. Если индекс массива не существует, то он выводит def 
+    return def
+  } else {
+    return cities[index] // Здесь в функцию возращается требуемый нам индекс 
+  }
 }
 
-console.log(calculateSum(coll1))
-console.log(calculateSum(coll2))
+// Проверка
+console.log(get(cities, 1)); // 'London'
+console.log(get(cities, 4)); // null
+console.log(get(cities, 10, 'Paris')); // 'Paris'
+console.log(get(cities, -1), 'Oops'); // 'Oops'
+console.log(get(cities, 11, 'Tomsk')); // Tomsk
 
+
+
+// Задача 2
+// Напишите функцию,  которая разбивает слова в регистре.
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// "" =>  ""
+
+// Для решения данной задачи использовался метод split (https://devacademy.ru/article/izuchaem-metod-split-v-javascript-na-primerakh)
+
+function split(string) {     // объявляем функцию split с получаемым аргументом string (строка)
+  let mySplit = string.split(/(?=[A-Z])/); // для того , чтобы метод split отработал нам нужно прописать условия после которых он отработает. В задаче требуется, разделить слова по заглавным буквам слов
+  return mySplit 
+}
+
+// Проверка
+console.log(split('camelCasing')) // 'camel' 'Casing'
+console.log(split('identifier')) // 'identifier'
+console.log(split('doingSomethingBoring')) //'doing' Something' 'Boring'
+
+
+// Напишите функцию, которая возвращает сумму всех позитивных чисел.
+// По умолчанию сумма равна 0.
+
+// Сделал по видео из YouTube - https://www.youtube.com/watch?v=DkQc5t_LfSQ
+
+let Array = [1, -4, 7, 12] // Массив
+let sum = 0 // Создается переменная, которая будет принимать результат суммы положительных чисел
+
+// Не придумал других вариантов, кроме как посмотреть в инернете. В интеренете решение производятся через цикл, так же как и в книге по JS 
+
+for (let i = 0; i < Array.length; i++) {  // создаётся цикл (for) где (1) i = инкремент, который говорит нам о то, что это цикл. (2) i < Array.length - тут показывается, количество шагов, которые выолнятся в цикле. (3) i++ - тут мы показываем, каким образом будет производиться переход в данном массиве 
+
+  if (Array[i] > 0) {   // Прописываем условия цикла. Нам требуется найти сумму положительных чисел и для этого задаем условие if (Arrai[i] > 0) - эта строчка производит поиск в массиве чисел которые больше значения (0). 
+
+    sum += Array[i] // Тут прописывается уже непосредствоено действие с найдеными в массиве числами.
+  }
+}
+console.log(sum) //20
