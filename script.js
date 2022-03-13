a9cfe29e97b7200909da2e0f193e775c6037b398
Anton Bekhtgold <jacepwncere@yandex.ru>
2022-03-13 22:09:06 +0300

    hw-6 done

--- script.js
+++ script.js
@@ -1,94 +1,32 @@
-// let string = "45385593107843568"
+let oddOrEven = array => {
+    let result = array.reduce((total,current) => {
+    return total + current;
+    },0);
 
-// function fakeStr(string) {
-//   let result = "";
-//   for (let i = 0; string[i]; i++) {
-//     if (string[i] < 5) {
-//       result += "0"
-//     } else(result[i] > 0)
-//       result += "1"
-//     }
-//     return result;
-//   }
+   if (result % 2 === 0){
+       return 'even';
+    } else {
+        return 'odd';
+    }
+}
 
-
-// console.log(fakeStr(string));
-
-// function sumElements(items) {
-//   result = 0;
-//   // items.forEach(function (item){
-//   //    result += Number.parseInt(item, 10);
-//   // })
-//   // return result
-
-//   items.reduce(function (total, current){
-//     return Number.parseInt(total, 10) + current
-//   },0);
-//   return result;
-
-//   // for (let item of items){
-//   //   result += Number.parseInt(item, 10);
-//   // }
-//   // return result
-// }
-
-// console.log(sumElements([9, 3, '7', '3'])); // 22
-// console.log(sumElements(['5', '0', 9, 3, 2, 1, '9', 6, 7])); // 42
-
-
-// function wave(str){
-//  let resultArr = [];
-//   for (let i = 0; i < str.length; i++ ){
-//     let result = str.split("");
-//     if (result[i] !== ""){
-//       result[i] = result[i].toUpperCase();
-//       resultArr.push(result.join(''));
-//     }
-//   }
-//   return resultArr
-// }
+console.log(oddOrEven([0, 1, 5])); // 'even'
+console.log(oddOrEven([0, 1, 3])); // 'even'
+console.log(oddOrEven([0, -1, -5])); // 'even'
+console.log(oddOrEven([0, 1, 4])); // 'odd'
+console.log(oddOrEven([0, -1, 2])); // 'odd'
+console.log(oddOrEven([0])); // 'even'
 
 
 
-// console.log(wave("hello"))
 
-// function capitalize(word) {
-// 	// let result = word.toLowerCase()
-//     //  return result[0].toUpperCase() + result.substring(1)
-//     return word[0].toUpperCase() + word.substring(1).toLowerCase()
-// }
-
-// // Sample usage - do not modify
-// console.log(capitalize("sam")); // "Sam"
-// console.log(capitalize("ALEX")); // "Alex"
-// console.log(capitalize("chARLie")); // "Charlie"
-
-// function convertNumberToString(number) {
-// 	// your code
-//     let str = number.toString()
-//     return number
-// }
-
-// // Sample usage - do not modify
-// console.log(convertNumberToString(42)); // "42"
-// console.log(convertNumberToString(97)); // "97"
-// console.log(convertNumberToString(11)); // "11
+let toJadenCase = array => {
+    let word = array.split(' ');
+    let result = word.map(item => {
+      return  item[0].toUpperCase() + item.slice(1);
+    }).join(' ');
+    return result;
+}
 
 
-// function sumMix (x){
-//   let result = 0;  
-//   x.forEach(function (el){
-//     result += Number.parseInt(el, 10);
-//   })
-//   return result
-// }
-
-
-// function noSpace(x) {
-//   return  x.split(' ').join('')
-  
-// }
-// console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')); // '8j8mBliB8gimjB8B8jlB'
-// console.log(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')); // '88Bifk8hB8BB8BBBB888chl8BhBfd'
-
-
+console.log(toJadenCase('How can mirrors be real if our eyes aren"t real')); // How Can Mirrors Be Real If Our Eyes Aren"t Real
